431cb3c82220ac629c8efcf9a3aa197e497c2a31
Kevin Duraj <kevinduraj@gmail.com>
2014-09-25 10:44:14 -0700

    multiple allocation for sequence files

--- ReadSequenceFile/src/main/java/kduraj/App.java
+++ ReadSequenceFile/src/main/java/kduraj/App.java
@@ -21,16 +21,20 @@ public class App {
         Path path = new Path(uri);
 
         SequenceFile.Reader reader = null;
+        
         try {
             reader = new SequenceFile.Reader(fs, path, conf);
             Writable key = (Writable) ReflectionUtils.newInstance(reader.getKeyClass(), conf);
             Writable value = (Writable) ReflectionUtils.newInstance(reader.getValueClass(), conf);
             long position = reader.getPosition();
+            
             while (reader.next(key, value)) {
+                
                 String syncSeen = reader.syncSeen() ? "*" : "";
                 System.out.printf("[%s%s]\t%s\t%s\n", position, syncSeen, key, value);
                 position = reader.getPosition();
             }
+            
         } catch (Exception ex) {
             System.out.println("ex = " + ex);
         } finally {
--- WriteSequenceFile/src/main/java/kduraj/App.java
+++ WriteSequenceFile/src/main/java/kduraj/App.java
@@ -6,38 +6,119 @@ import java.net.URI;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IOUtils;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.SequenceFile;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.*;
 
-public class App 
-{
-    public static void main( String[] args ) throws IOException
-    {
-        String[] DATA = {"One, two, bucle my shoe", "Three, four, shut the door", "five, six, pick up sticks"};
+public class App {
 
+    /*--------------------------------------------------------------------------------------------*/
+    String[] DATA = {
+        "one, first line", "two, second line", "tree, third line",
+        "four, forth line", "five, fifth line", "six, sixth line",
+        "four, forth line", "five, fifth line", "six, sixth line",};
+
+    static String uri;
+    static Configuration conf;
+    static FileSystem fs;
+    static Path path;
+
+    /*--------------------------------------------------------------------------------------------    
         String uri = args[0];
         Configuration conf = new Configuration();
         FileSystem fs = FileSystem.get(URI.create(uri), conf);
         Path path = new Path(uri);
+     --------------------------------------------------------------------------------------------*/
+    public static void main(String[] args) throws IOException {
+        
+        uri = args[0];
+        conf = new Configuration();
+        fs = FileSystem.get(URI.create(uri), conf);
+        path = new Path(uri);
+        
+        App app = new App();        
+        app.key_val_bytes();
+    }
 
-        IntWritable key = new IntWritable();
+    /*--------------------------------------------------------------------------------------------*/
+    public void key_val_bytes() throws IOException {
+
+        BytesWritable key = new BytesWritable();
         Text value = new Text();
+
         SequenceFile.Writer writer = null;
 
         try {
             writer = new SequenceFile.Writer(fs, conf, path, key.getClass(), value.getClass());
-            for (int i = 0; i < 100; i++) {
-                key.set(100 - i);
-                value.set(DATA[i % DATA.length]);
-                //System.out.printf("[%s]\t%s\t%s\n", writer.getLength(), key, value);
-                writer.append(key, value);
+
+            for (int i = 0; i < DATA.length; i++) {
+
+                String[] parts = DATA[i].split(",");
+                writer.append(new BytesWritable(parts[0].getBytes()), value);
+
             }
+
         } catch (Exception ex) {
             System.out.println("Error writing: " + ex.getMessage());
         } finally {
             IOUtils.closeStream(writer);
         }
     }
+    /*--------------------------------------------------------------------------------------------*/
+
+    public void key_val_text(String[] args) throws IOException {
+
+        Text key = new Text();
+        Text value = new Text();
+
+        SequenceFile.Writer writer = null;
+
+        try {
+            writer = new SequenceFile.Writer(fs, conf, path, key.getClass(), value.getClass());
+
+            for (int i = 0; i < DATA.length; i++) {
+
+                String[] parts = DATA[i].split(",");
+                key.set(parts[0]);
+                value.set(DATA[i % DATA.length]);
+
+                System.out.printf("[%s]\t%s\t%s\n", writer.getLength(), key, value);
+                writer.append(key, value);
+
+            }
+
+        } catch (Exception ex) {
+            System.out.println("Error writing: " + ex.getMessage());
+        } finally {
+            IOUtils.closeStream(writer);
+        }
+    }
+    /*--------------------------------------------------------------------------------------------*/
+
+    public void key_val_integer(String[] args) throws IOException {
+
+        IntWritable key = new IntWritable();
+        Text value = new Text();
+
+        SequenceFile.Writer writer = null;
+
+        try {
+            writer = new SequenceFile.Writer(fs, conf, path, key.getClass(), value.getClass());
+
+            for (int i = 0; i < DATA.length; i++) {
+
+                String[] parts = DATA[i].split(",");
+                key.set(i);
+                value.set(DATA[i % DATA.length]);
+
+                System.out.printf("[%s]\t%s\t%s\n", writer.getLength(), key, value);
+                writer.append(key, value);
+
+            }
+
+        } catch (Exception ex) {
+            System.out.println("Error writing: " + ex.getMessage());
+        } finally {
+            IOUtils.closeStream(writer);
+        }
+    }
+    /*--------------------------------------------------------------------------------------------*/
 }
